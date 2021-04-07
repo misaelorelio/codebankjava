1cafd065838956154e1ee7280761a0f8e1efe1a0
misaelorelio <misaelorelio@hotmail.com>
2021-04-07 13:30:08 -0300

    validação da idade > ou idade < que 18 anos e outras correções

--- src/main/java/com/bank/api/controller/ContaController.java
+++ src/main/java/com/bank/api/controller/ContaController.java
@@ -1,10 +1,16 @@
 package com.bank.api.controller;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,8 +33,20 @@ public class ContaController {
 	}
 
 	@PostMapping("/adicionar")
-	public Conta adicionar(@Valid @RequestBody Conta conta) {
-		return ContaRepository.save(conta);
+	public ResponseEntity  adicionar(@Valid @RequestBody Conta conta) {
+		LocalDate dataAtual = LocalDate.now();
+		LocalDate dataNascimento = conta.getDataNascimento().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();;
+		int idade = Period.between(dataNascimento, dataAtual).getYears();
+		
+		if(idade >= 18 ) {
+			return ResponseEntity.status(HttpStatus.OK).body(ContaRepository.save(conta));  
+		}else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{message: 'Não é possível incluir uma conta bancária para clientes de menor idade'}"); 
+		}
+		
+		
+		
 	}
+	
 
 }
